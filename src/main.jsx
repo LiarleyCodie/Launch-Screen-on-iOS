a158956a2c1e8c58e99c5eaec0020670c8012e5a
liarleycodie <liarley.personal@gmail.com>
2023-06-10 21:56:15 -0300

    init: junk files removed and components and pages folder created

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Login } from './pages/Login/index'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
